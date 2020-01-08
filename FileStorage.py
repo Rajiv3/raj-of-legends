6bd3dc2edec48e22f4b12c3d86ab0d3be1d7d483
rajiv3 <rajivchattopadhyay@hotmail.com>
2020-01-08 18:52:50 -0500

    new way to manage database storage?

---
+++ FileStorage.py
@@ -0,0 +1,9 @@
+import os
+
+class FileStorage:
+    """class to manage where the data files are to be stored"""
+    # use the path module
+
+    def __init__(self):
+
+        self.dataStorage = f"C:/raj-of-legends-data"
